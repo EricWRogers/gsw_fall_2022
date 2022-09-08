721c0a03412182c4df25473e01956759f1e672c8
Logan <Loganhobson31@gmail.com>
2022-09-08 15:38:26 -0500

    Build .000001
    Changed main menu script. Wired things into Logans Test scenes for the build. Added Logans Test scene into the build settings.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
    public void PlayGame ()
    {
-		  SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		  SceneManager.LoadScene("Logan'sTestScenePath"); //Name of the Scene
    }
    public void QuitGame ()
    {
