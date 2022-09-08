5ba11c429d3961da2c0a9c0b717f2afdb4769233
Duel <duelifendley@gmail.com>
2022-09-08 13:58:29 -0500

    aoe, damage, durability

---
+++ Assets/Scripts/CombatScripts/WeaponManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public bool aoecheck = false;
+    public WeaponScriptableObjects CurrentWeapon;
+    public GameObject HitBoxOffset;
+    
+    //sets the hitbox to true with an input
+    public void HitEnemies()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<CircleCollider2D>().enabled = true;
+            aoecheck = true;
+            Debug.Log("In Hit Function");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(aoecheck) { 
+        //checks for contact with the enemy tag
+        Debug.Log("In Trigger Function");
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //adjusts health
+            collision.gameObject.GetComponent<EnemyHitPoint>().CurrentHP -= CurrentWeapon.WeaponDamage;
+
+            //weapon will break
+            CurrentWeapon.WeaponDurability -= 1;
+            if (CurrentWeapon.WeaponDurability <= 0)
+                Debug.Log("Weapon Broke");
+
+            //enemy will be removed if health is gone
+            if (collision.gameObject.GetComponent<EnemyHitPoint>().CurrentHP <= 0)
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+        //hitbox is set back to false
+        aoecheck = false;
+            GetComponent<CircleCollider2D>().enabled = false;
+        }
+    }
+    
+    //running to be able to turn hitbox on
+    void Update()
+    {
+        HitEnemies();
+
+    }
+}
