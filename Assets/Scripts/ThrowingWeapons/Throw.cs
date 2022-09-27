77e9b2956d766941bdfd65e978c9e5a56ba9e3a6
Peyton <pjbowden53@gmail.com>
2022-09-27 15:33:56 -0500

    throwing knife now damages enemies

--- Assets/Scripts/ThrowingWeapons/Throw.cs
+++ Assets/Scripts/ThrowingWeapons/Throw.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class Throw : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
-
+    public ThrowingKnife TK;
+    public GameObject player;
 
     [HideInInspector] public float KnifeVelocity;
+    private void Start()
+    {
+        TK = player.GetComponent<ThrowingKnife>();
+    }
     private void FixedUpdate()
     {
         rb.velocity = transform.up * KnifeVelocity;
         Destroy(gameObject, 2f);  //how long the objects stay in the scene before getting deleted if they don't hit anything
                                   //in this case 2 seconds before they disappear
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Enemy Attacked");
+            //Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+            //enemy.health -= 100;
+            //enemy.TakeDamage(knifeDamage);
+            collision.gameObject.GetComponent<SuperPupSystems.Helper.Health>().Damage(TK.knifeDamage); //Logans Code. Works with Erics Health Script.
+            Debug.Log(collision);
+        }
+        Destroy(gameObject); //destroys knives when they hit something
     }
 }
