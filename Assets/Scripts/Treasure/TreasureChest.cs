ca8bf61ce1f7c1ba402111bd895b133cc8ef7e1f
Duel <duelifendley@gmail.com>
2022-09-27 22:05:45 -0500

    chest

--- Assets/Scripts/Treasure/TreasureChest.cs
+++ Assets/Scripts/Treasure/TreasureChest.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TreasureChest : MonoBehaviour
+
 {
+    private bool isOpened;
+    
     public GameObject[] lootTable;
     void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && isOpened == false)
         {
             if (Input.GetKeyDown(KeyCode.X))
             {
                 DropItem();
+                isOpened = true;
             }
         }
 
