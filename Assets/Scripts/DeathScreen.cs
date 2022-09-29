8a399b34a66baebc38c470ed08feecc53fe81db4
Katie Gordon <katiejewel7@gmail.com>
2022-09-29 14:21:48 -0500

    Set time scale to 0 to pause the gameplay when the player runs out of health or fails

--- Assets/Scripts/DeathScreen.cs
+++ Assets/Scripts/DeathScreen.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DeathScreen : MonoBehaviour
-{
-    public void YouDied ()
-   {
-		  SceneManager.LoadScene("KatiesTestScene"); //Name of the Scene
-   }
+public class DeathScreen : MonoBehaviour{
+
+	public static bool dead = false;
+
+	public GameObject deathMenuUI;
+	
+	void Update(){
+		if(dead)
+		{
+			GameOver();
+			SceneManager.LoadScene("KatiesTestScene");//Name of the Scene
+		}
+		
+	}
+	void GameOver ()
+	{
+		deathMenuUI.SetActive(true);
+		Time.timeScale = 0.0f;
+		dead = true;
+	}
 }
