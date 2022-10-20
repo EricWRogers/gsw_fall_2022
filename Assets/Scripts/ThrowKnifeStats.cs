256e9d201cddf03bd8d85585f3833ab607319156
Kaylie Tipton <kaylietipton23@gmail.com>
2022-10-20 14:40:28 -0500

    Throwing Knife script Stats

---
+++ Assets/Scripts/ThrowKnifeStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Throwing Knife", menuName = "Items/Throwing Knife")]
+public class ThrowKnifeStats : ItemStats
+{
+    public int damage;
+    public int range;
+    public int speed;
+    
+}
