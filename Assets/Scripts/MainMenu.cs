6fed7942f87ec940407d34dae3f88a391f1d5889
JTAdams <jtadams6501@gmail.com>
2022-11-13 11:32:39 -0600

    Most Ui Audios are in (should be)
    Music acting a lil buggy

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,17 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class MainMenu : MonoBehaviour
 {
-   public string sceneName; //We can now input the scene we want to go to
+    public AudioManager audio;
 
-   public void PlayGame ()
-   {
-		  SceneManager.LoadScene(sceneName); //Name of the Scene
-   }
-   public void QuitGame ()
-   {
-		  Debug.Log("QUIT!");
-		  Application.Quit();
-   }
+    public string sceneName; //We can now input the scene we want to go to
+    void Update()
+    {
+        Play();
+        PlayMusic();
+    }
+    void Awake()
+    {
+
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneName); //Name of the Scene
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+    private void Play()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+
+            AudioSource sound = GameObject.Find("sound_9_uiButton").GetComponent<AudioSource>();
+            sound.Play();
+        }
+        else
+        {
+            return;
+        }
+    }
+    private void PlayMusic()
+    {
+        
+            AudioSource sound = GameObject.Find("sound_0_Title Music").GetComponent<AudioSource>();
+            sound.Play();
+        
+    }
 }
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,38 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour{
+public class PauseMenu : MonoBehaviour
+{
 
-   public static bool GameIsPaused = false;
+	public static bool GameIsPaused = false;
 
-   public GameObject pauseMenuUI;
+	public AudioManager audio;
+	public GameObject pauseMenuUI;
 
-   void Update(){
-		  if(Input.GetKeyDown(KeyCode.Escape))
-		  {
-				if (GameIsPaused)
-				{
-					Resume();
-				}
-				else
-				{
-					Pause();
-				}
-		  }
-   }
-   void Resume ()
-   {
-		  pauseMenuUI.SetActive(false);
-		  Time.timeScale = 1.0f;
-		  GameIsPaused = false;
-   }
+	void Update()
+	{
 
-   void Pause()
-   {
-		  pauseMenuUI.SetActive(true);
-		  Time.timeScale = 0.0f;
-		  GameIsPaused = true;
-   }
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (GameIsPaused)
+			{
+				Resume();
+				
+			}
+			else
+			{
+				Pause();
+			}
+		}
+	}
+	void Resume()
+	{
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1.0f;
+		GameIsPaused = false;
+	}
+
+	void Pause()
+	{
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0.0f;
+		GameIsPaused = true;
+	}
+
+}
 
    
-}
+
