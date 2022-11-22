540f3f5f9ac55ce70d09adfec5e72f2c8c95ee86
Peyton <pjbowden53@gmail.com>
2022-11-22 15:36:10 -0600

    should be there

--- Assets/Scripts/ThrowingWeapons/ParticleEffectManager.cs
+++ Assets/Scripts/ThrowingWeapons/ParticleEffectManager.cs
@@ -8,6 +8,25 @@ public class ParticleEffectManager : MonoBehaviour
     private ParticleSystem damagePFX;
 
 
+
+    public void initilizeEffect()
+    {
+        if (deathPFX == null)
+        {
+            damagePFX = transform.Find("EnemyHitEffect").GetComponent<ParticleSystem>();
+            deathPFX = transform.Find("EnemyDeathEffect").GetComponent<ParticleSystem>();
+        }
+    }
+    public void playDeathPFX()
+    {
+        deathPFX.Play();
+    }
+
+    public void playDamagePFX()
+    {
+        damagePFX.Play();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
