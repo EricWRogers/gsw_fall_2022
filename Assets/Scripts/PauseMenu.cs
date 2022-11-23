646f47967ae66aae1cda126509ec9df231c50dc7
JTAdams <jtadams6501@gmail.com>
2022-11-23 01:15:22 -0600

    Pause Menu Clicks

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -10,18 +10,26 @@ public class PauseMenu : MonoBehaviour
 	public AudioManager audio;
 	public GameObject pauseMenuUI;
 
+
+	void Start()
+	{
+		//PlayMusic();
+	}
+
 	void Update()
 	{
 
 		if (Input.GetKeyDown(KeyCode.Escape))
 		{
+			Play();
 			if (GameIsPaused)
 			{
 				Resume();
-				
+
 			}
 			else
 			{
+
 				Pause();
 			}
 		}
@@ -35,12 +43,36 @@ public class PauseMenu : MonoBehaviour
 
 	void Pause()
 	{
+
+
 		pauseMenuUI.SetActive(true);
 		Time.timeScale = 0.0f;
 		GameIsPaused = true;
 	}
 
-}
+	private void Play()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Debug.Log("No Sound Called");
+			AudioSource sound = GameObject.Find("sound_9_uiButton").GetComponent<AudioSource>();
+			sound.Play();
+		}
+		else
+		{
 
-   
+			return;
+		}
+	}
+		/*private void PlayMusic()
+		{
+			Debug.Log("Eric Look " + name);
+			AudioSource sound = GameObject.Find("sound_2_uiBGMusic").GetComponent<AudioSource>();
+			sound.Play();
+
+		} */
+	}
+
+
+
 
